4325603f037ea0a66516dc2a56e588448e364934
luis-domingues <luisdomingues117@gmail.com>
2024-09-30 15:49:53 -0300

    feat: add initial DbContext for database connection

---
+++ Context/SocialNetApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetApi.Entities;
+
+namespace SocialNetApi.Context;
+
+public class SocialNetApiContext : DbContext
+{
+    public SocialNetApiContext(DbContextOptions<SocialNetApiContext> options) : base(options) {  }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+}
