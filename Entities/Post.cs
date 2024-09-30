5105c2267de71210b7ba9ec0d3f5f77255c7ce7d
luis-domingues <luisdomingues117@gmail.com>
2024-09-30 15:52:39 -0300

    feat: add Posts collection to User and update Post entity with User navigation

---
+++ Entities/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialNetApi.Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialNetApi.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public ICollection<Post> Posts { get; set; } = new List<Post>();
+}
