ad091c097c9c93d2dcfcc5a66e1b2ae40bc3ae80
luis-domingues <luisdomingues117@gmail.com>
2024-10-02 14:11:15 -0300

    feat: add Comment entity with relationships to User and Post

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -2,5 +2,10 @@
 
 public class Comment
 {
-    
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public int PostId { get; set; }
+    public Post Post { get; set; }
 }
